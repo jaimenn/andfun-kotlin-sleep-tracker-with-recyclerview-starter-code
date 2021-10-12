c23d6d167515a9615d2a64b7a39b69b31125af55
Jaime Nejaim <jaime_nejaim_ext@carrefour.com>
2021-10-12 18:11:49 -0300

    adding onClickListener in sleepnight list

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -20,15 +20,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
 import com.example.android.trackmysleepquality.sleeptracker.adapter.SleepNightAdapter
+import com.example.android.trackmysleepquality.sleeptracker.adapter.SleepNightListener
 import com.google.android.material.snackbar.Snackbar
 
 /**
@@ -101,8 +104,12 @@ class SleepTrackerFragment : Fragment() {
             }
         })
 
-        adapter = SleepNightAdapter()
+        adapter = SleepNightAdapter(SleepNightListener { nightId ->
+            Toast.makeText(context, "${nightId}", Toast.LENGTH_LONG).show()
+        })
 
+        val manager = GridLayoutManager(activity, 3)
+        binding.sleepList.layoutManager = manager
         binding.sleepList.adapter = adapter
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
